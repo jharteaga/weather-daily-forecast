4f7c1e0e8925b6a7e5ff00516c2cb7f62d5a58b7
jharteaga <jh.arteaga09@gmail.com>
2020-03-19 19:33:12 -0700

    Get Forecast information completed

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 //Controls
 const searchBtn = document.querySelector('.fa-search');
-const city = document.getElementById('city').value;
+const city = document.getElementById('city');
 const metric = document.querySelector('.metrics-container .selected');
 const updateBtn = document.getElementById('update');
+const metricC = document.getElementById('celsious');
+const metricF = document.getElementById('farenheit');
+const cityForm = document.querySelector('.cities');
+let tempMetric = 'metric';
 
 //Information
 const tempNumber = document.querySelector('.tempNumber');
 const tempDesc = document.querySelector('.tempDesc');
 const infoDescArr = document.querySelectorAll('.info-desc');
 
-searchBtn.addEventListener('click', () => {
+getForecast();
+
+//Get forecast information
+function getForecast() {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=71781884cf70876dbf9f5326ed95d5a1&units=metric`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city.value},us&appid=71781884cf70876dbf9f5326ed95d5a1&units=${tempMetric}`,
     {
       method: 'get'
     }
@@ -25,4 +32,27 @@ searchBtn.addEventListener('click', () => {
       infoDescArr[1].textContent = data.main.humidity + '%';
       infoDescArr[2].textContent = data.main.feels_like;
     });
+}
+
+cityForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getForecast();
+});
+
+searchBtn.addEventListener('click', () => {
+  getForecast();
+});
+
+metricC.addEventListener('click', () => {
+  metricC.classList.add('selected');
+  metricF.classList.remove('selected');
+  tempMetric = 'metric';
+  getForecast();
+});
+
+metricF.addEventListener('click', () => {
+  metricF.classList.add('selected');
+  metricC.classList.remove('selected');
+  tempMetric = 'imperial';
+  getForecast();
 });
